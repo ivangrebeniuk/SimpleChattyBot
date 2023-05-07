aa45bda192ac69b395a986d8b15a48aa63ce8fd3
Ivan Grebeniuk <i.grebeniuk1@gmail.com>
2023-05-07 16:06:13 +0400

    Lesson 3 done: added feature to guess user's age

--- ChattyBot/src/main/kotlin/Main.kt
+++ ChattyBot/src/main/kotlin/Main.kt
@@ -7,8 +7,16 @@ fun main() {
 
     println("Hello! My name is Patric.\nI was created in 2023.")
     println("Please, remind me your name.")
+    val name = scanner.nextLine()
+    println("What a great name you have, $name!")
 
-    val userName = scanner.nextLine()
-    println("What a great name you have, $userName!")
+    // Guessing the age
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val reminderThree = readln().toInt()
+    val reminderFive = readln().toInt()
+    val reminderSeven = readln().toInt()
 
+    val yourAge = (reminderThree * 70 + reminderFive * 21 + reminderSeven * 15) % 105
+    println("$name, your age is $yourAge, it's a great time to start learning Kotlin!")
 }
