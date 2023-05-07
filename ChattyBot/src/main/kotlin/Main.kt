a626ca310f085b992ef983b9f2b1eb4f465d17ce
Ivan Grebeniuk <i.grebeniuk1@gmail.com>
2023-05-07 15:34:00 +0400

    Lesson 2, bot prints username

--- ChattyBot/src/main/kotlin/Main.kt
+++ ChattyBot/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+package bot
+
+import java.util.Scanner
 fun main() {
+
+    val scanner = Scanner(System.`in`)
+
     println("Hello! My name is Patric.\nI was created in 2023.")
+    println("Please, remind me your name.")
+
+    val userName = scanner.nextLine()
+    println("What a great name you have, $userName!")
+
 }
