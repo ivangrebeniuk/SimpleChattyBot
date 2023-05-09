2b6f6d0e49d625bdc45db82422f17b06adf1d364
Ivan Grebeniuk <i.grebeniuk1@gmail.com>
2023-05-09 22:08:32 +0400

    Lesson 4: addad counting feature

--- ChattyBot/src/main/kotlin/Main.kt
+++ ChattyBot/src/main/kotlin/Main.kt
@@ -2,15 +2,16 @@ package bot
 
 import java.util.Scanner
 fun main() {
-
+    // Lesson 1 Introduce bot
     val scanner = Scanner(System.`in`)
-
     println("Hello! My name is Patric.\nI was created in 2023.")
     println("Please, remind me your name.")
+
+    // Lesson 2. Print username
     val name = scanner.nextLine()
     println("What a great name you have, $name!")
 
-    // Guessing the age
+    // Lesson 3 Guessing the age
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
     val reminderThree = readln().toInt()
@@ -19,4 +20,14 @@ fun main() {
 
     val yourAge = (reminderThree * 70 + reminderFive * 21 + reminderSeven * 15) % 105
     println("$name, your age is $yourAge, it's a great time to start learning Kotlin!")
+
+    // Lesson 4. Count the numbers
+    println("Now I will prove to you that I can count to any number you want.")
+    val number = readln().toInt()
+
+    for (i in 1..number) {
+        println("$i!")
+    }
+
+    println("Completed, have a nice day!")
 }
